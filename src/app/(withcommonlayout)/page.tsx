caf4f40bb2c1f6dbee7f8cd44343d7a97957c45f
dipujafar <dipujafarmr@gmail.com>
2024-11-01 23:45:59 +0600

    added  landing and react posts section in home page

--- src/app/(withcommonlayout)/page.tsx
+++ src/app/(withcommonlayout)/page.tsx
@@ -1,16 +1,11 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/src/config/site";
-import { title, subtitle } from "@/src/components/primitives";
-import { GithubIcon } from "@/src/components/icons";
+import Landing from "@/src/components/modules/home/Landing";
+import RecentPosts from "@/src/components/modules/home/RecentPosts";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
-      <h1>Found X</h1>
-    </div>
+    <>
+      <RecentPosts />
+      <Landing />
+    </>
   );
 }
---
+++ src/components/modules/home/RecentPosts.tsx
@@ -0,0 +1,9 @@
+const RecentPosts = () => {
+  return (
+    <div>
+      <h1>This is Recent post section</h1>
+    </div>
+  );
+};
+
+export default RecentPosts;
