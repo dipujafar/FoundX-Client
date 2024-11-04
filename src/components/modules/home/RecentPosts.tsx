055d8eecb2f9d63a699321e4a1c09a840c16785e
dipujafar <dipujafarmr@gmail.com>
2024-11-04 23:52:56 +0600

    get recent post data from backend with server side fetching

--- src/components/modules/home/RecentPosts.tsx
+++ src/components/modules/home/RecentPosts.tsx
@@ -1,8 +1,29 @@
-const RecentPosts = () => {
+import { Button } from "@nextui-org/button";
+import Container from "../../ui/Container";
+import Link from "next/link";
+import { getResentPosts } from "@/src/services/recentPosts";
+
+const RecentPosts = async () => {
+  const data = await getResentPosts();
+
+  console.log("data", data);
   return (
-    <div>
-      <h1>This is Recent post section</h1>
-    </div>
+    <Container>
+      <div className="lg:my-8 my-4">
+        <h2 className="mb-2 text-center text-2xl">Recently Found Items</h2>
+        <p className="text-center">
+          A list of items that have been recently found and reported
+        </p>
+      </div>
+      <div className="lg:my-8 my-4 grid justify-center gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <h1>Recent Posts</h1>
+      </div>
+      <div className="flex justify-center">
+        <Button className="rounded-md bg-default-900 text-default" size="md">
+          <Link href={"/found-items"}>See All</Link>
+        </Button>
+      </div>
+    </Container>
   );
 };
 
